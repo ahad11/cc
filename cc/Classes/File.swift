fde41fe8f872181fbbae2e2ec0c28a006dcdeb5a
ahad375 <aalarifi64@gmail.com>
2017-11-05 10:01:03 +0300

    initial

---
+++ cc/Classes/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  cc
+//
+//  Created by ahad on 11/5/17.
+//
+
+import Foundation
+public class elm{
+    
+    public func elmsoft(){
+        print("elm soft")
+    }
+    
+    
+}
